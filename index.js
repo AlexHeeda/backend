1b787078bed0e1cfb45f2351eeef0d097e279373
Alex <alex.gauthier41@gmail.com>
2018-12-10 11:18:24 +0100

    maj

--- index.js
+++ index.js
@@ -2,20 +2,43 @@ const connection = require('./conf');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Session = require('express-session');
+const FileStore = require('session-file-store')(Session);
 const port = 3000;
+
+app.use(Session({
+    store: new FileStore({
+        // path: path.join(__dirname, '/tmp'),
+        encrypt: true
+    }),
+    secret: 'Super Secret !',
+    resave: true,
+    saveUninitialized: true,
+    name: 'sessionId'
+}));
 
 app.use(bodyParser.json());
 // Support URL-encoded bodies
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
+
+app.get("/session-in", (req, res) => {
+    req.session.song = "be bop a lula.";
+    res.send("ok");
+});
+
+app.get("/session-out", (req, res) =>{
+    res.send(req.session.song);
+});
 
 /**
  * 1.Récupére toute les données.
  */
 app.get("/all", (req, res) => {
+    req.session.maVariable = true; 
     connection.query('SELECT * FROM wcs_api', (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu récupérer les données")
         } else {
             res.json(result);
@@ -28,7 +51,7 @@ app.get("/all", (req, res) => {
  */
 app.get("/all/light", (req, res) => {
     connection.query('SELECT name, jour FROM wcs_api', (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu récupérer les données")
         } else {
             res.json(result);
@@ -41,7 +64,7 @@ app.get("/all/light", (req, res) => {
  */
 app.get("/all/wcs", (req, res) => {
     connection.query('SELECT * FROM wcs_api WHERE name LIKE "%wcs%"', (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu récupérer les données")
         } else {
             res.json(result);
@@ -55,7 +78,7 @@ app.get("/all/wcs", (req, res) => {
  */
 app.get("/all/campus", (req, res) => {
     connection.query('SELECT * FROM wcs_api WHERE name LIKE "campus%"', (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu récupérer les données")
         } else {
             res.json(result);
@@ -68,7 +91,7 @@ app.get("/all/campus", (req, res) => {
  */
 app.get("/all/date", (req, res) => {
     connection.query('SELECT * FROM wcs_api WHERE jour > "2010/01/01"', (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu récupérer les données")
         } else {
             res.json(result);
@@ -84,7 +107,7 @@ app.get("/all/:choice", (req, res) => {
     const choix = req.params.choice;
     console.log(choix);
     connection.query(`SELECT * FROM wcs_api ORDER BY id_api ${choix}`, (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu récupérer les données")
         } else {
             res.json(result);
@@ -95,12 +118,12 @@ app.get("/all/:choice", (req, res) => {
 /**
  * 7.Ajoute des données a la base.
  */
-app.post("/all/save" , (req, res) => {
+app.post("/all/save", (req, res) => {
     // récupération des données envoyées
     const formData = req.body;
 
     connection.query('INSERT INTO wcs_api SET ?', formData, (err, result) => {
-        if(err) {
+        if (err) {
             res.status(500).send("nous n'avons pas pu inserer les données")
         } else {
             res.send("Données ajouté");
@@ -118,37 +141,36 @@ app.put("/all/change/:id", (req, res) => {
 
     // connection à la base de données, et insertion de l'employé
     connection.query('UPDATE wcs_api SET ? WHERE id_wcs = ?', [formData, idW], err => {
-  
+
         if (err) {
-          // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-          console.log(err);
-          res.status(500).send("nous n'avons pas pu inserer les données");
+            // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+            console.log(err);
+            res.status(500).send("nous n'avons pas pu inserer les données");
         } else {
-    
-          // Si tout s'est bien passé, on envoie un statut "ok".
-          res.send("Données ajouté");
+
+            // Si tout s'est bien passé, on envoie un statut "ok".
+            res.send("Données ajouté");
         }
-      });
     });
+});
 
 
 
 /**
  * 9.Toggle du booléen, change la valeur par l'url, si rentrez true alors false et inversement.
  */
-app.put("/all/:toggle/:id", (req, res) => {
+app.put("/all/toggle/:id", (req, res) => {
     const idwcs = req.params.id;
-    const resultt = req.params.toggle;
-    connection.query(`UPDATE wcs_api SET answer = ${resultt} WHERE id_wcs = ?`,  idwcs, err => {
+    connection.query(`UPDATE wcs_api SET answer = !answer WHERE id_wcs = ?`, idwcs, err => {
         if (err) {
             // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
             console.log(err);
             res.status(500).send("nous n'avons pas pu inserer les données");
-          } else {
-      
+        } else {
+
             // Si tout s'est bien passé, on envoie un statut "ok".
             res.send("Données ajouté");
-          }
+        }
     });
 })
 
@@ -162,9 +184,9 @@ app.delete("/all/delete/:id", (req, res) => {
         if (err) {
             console.log(err);
             res.status(500).send("Erreur dans la suppression");
-          } else {
+        } else {
             res.sendStatus(200);
-          }
+        }
     });
 });
 
@@ -178,9 +200,9 @@ app.delete("/all/deletew/", (req, res) => {
         if (err) {
             console.log(err);
             res.status(500).send("Erreur dans la suppressions");
-          } else {
+        } else {
             res.sendStatus(200);
-          }
+        }
     })
 })
 
